9d596cdb73426ce018ce2c3eb8a286859dbf2423
daniellutziger <daniel.lutziger@uzh.ch>
2026-04-15 18:16:26 +0200

    creation of the main file to test for the sensor

---
+++ imu_software/main.py
@@ -0,0 +1,19 @@
+import adafruit_bno055
+import board
+
+i2c = board.I2C()
+
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+while True:
+    # print all sensors
+    print("Temperature: {} degrees C".format(sensor.temperature))
+    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+    print("Gyroscope (degrees/sec): {}".format(sensor.gyro))
+    print("Euler angle: {}".format(sensor.euler))
+    print("Quaternion: {}".format(sensor.quaternion))
+    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+    print("Gravity (m/s^2): {}".format(sensor.gravity))
+
+
